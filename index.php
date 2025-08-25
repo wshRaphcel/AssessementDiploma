cd46d8c551db2e16e0419df62bb982409f0d03d8
wshRaphcel <lalilah92@gmail.com>
2025-08-25 01:48:39 +0000

    adding bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 require('vendor/autoload.php');
 
-use Rp\vscode\AssessementDiploma\App;
+use Vscode\AssessementDiploma\App;
 
 // create an app object based on App class 
 $app = new App();
+// print out the environment variables
+// print_r($_ENV);
+$title = "Home Page";
+$message = "Hello there";
+
+// create a template loader
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+// load the template into memore
+$template = $twig -> load('index.html.twig');
+// add some variables for twig to render
+echo $template -> render([
+    'title' => $title,
+    'message' => $message
+]);
 ?>
